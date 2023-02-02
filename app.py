e04b04e93f86fa20ca0a8079bb02a7c0127f40f3
3psprd <3psprd@3rdpartysolutions.com>
2023-02-02 13:39:11 -0500

    added the environment variable

--- app.py
+++ app.py
@@ -304,8 +304,8 @@ def hello():
 @app.route('/<name>')
 def hello_world(name=None):
 
-    # r = redis.from_url(os.environ['REDIS_URL'])
-    r = redis.from_url("redis://red-cfdksekgqg45rntqt0cg:6379")
+    r = redis.from_url(os.environ['REDIS_URL'])
+    # r = redis.from_url("redis://red-cfdksekgqg45rntqt0cg:6379")
 
 
     """ Connect to the PostgreSQL database server """
